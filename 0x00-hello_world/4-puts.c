bdb04ed96f31b1820f12cb0f0460fdf8437f90ed
Mahmoud-khairi <khairimahmoud01@gmail.com>
2023-03-16 07:10:33 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-/** 
+/**
  *main-Begin
  *
- *Description: "C Program print with puts" 
+ *Description: "C Program print with puts"
  *
  *Return: Always 0.
 */
@@ -12,4 +12,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
