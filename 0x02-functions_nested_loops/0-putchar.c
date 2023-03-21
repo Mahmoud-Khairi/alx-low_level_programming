080550d41ef09debab03ac6489b08f13d5d058f7
Mahmoud-khairi <khairimahmoud01@gmail.com>
2023-03-21 06:54:31 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,9 +11,8 @@ int main(void)
         char str[] = "_putchar";
         int i;
 
-        for (i = 0; i < 8; i++)
-                _putchar(str[i]);
-        _putchar('\n');
-
-        return (0);
-}  
+	for (i = 0; i < 8; i++)
+		 _putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
