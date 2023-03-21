96db41b2c6746ae1edb22878e13061848eb8db76
Mahmoud-Khairi <khairimahmoud01@gmail.com>
2023-03-21 22:46:43 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Ret: sum of a & b
+*/
+
+int add(int a, int b)
+{
+	int s = a + b;
+
+	return (s);
+}
