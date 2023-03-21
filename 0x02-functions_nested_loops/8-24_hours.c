d57896aec1446d5a35ce803ff0dd8b1584f5d2d6
Mahmoud-Khairi <khairimahmoud01@gmail.com>
2023-03-21 18:38:30 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point
+ * Description: prints the last digit of a number.
+ * Return: digit of a number
+ * @i: Ascii code
+ */
+void jack_bauer(void)
+{
+char ch[] = z+y+':'+x+i;
+int x,y,z,i;
+for (int i = 0 ; i < 10 ; i++)
+if ( i == 9)
+{
+i = 0;
+x = 1;
+}
+for (x = 1 ; x < 6 : x++)
+if ( x == 5)
+{
+x = 0;
+y = 1;
+}
+for (y = 1 ;  y < 10 : y++)
+if ( y == 9)
+{
+y = 0;
+z = 1;
+}
+for (z = 1 ; i < 3 : z++)
+if ( z == 2 && y == 3)
+{
+x = 0;
+y = 0;
+z = 0;
+i = 0;
+}
+_putchar(zyxi);
+}
