d746663aba377a952ad22d0ad3dd98f03ae83f0a
Mahmoud-khairi <khairimahmoud01@gmail.com>
2023-06-15 17:53:54 -0700

    fixing errors

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,21 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for ind_a in matrix:
-        new_matrix[len(new_matrix):] = [[elem ** 2 for elem in ind_a]]
-    return new_matrix
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: head of the dlistint_t list.
+ *
+ * Return: number of nodes..
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}x
